3fd652270cb2ecdba11533a3e9176a436ba8a69d
João Vinícius <vi.peres@icloud.com>
2022-01-14 15:02:01 -0300

    chore: version

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,19 +1,20 @@
-import { FC } from 'react';
+import { ReactNode } from 'react';
 
 import { ContainerProps, Container } from './styles';
 
 export interface GridProps extends ContainerProps {
   className?: string;
+  children?: ReactNode;
 }
 
-export const Grid: FC<GridProps> = ({
+export const Grid = ({
   children,
   className = '',
   columns,
   gap = 16,
   rows = '1fr',
   ...rest
-}) => {
+}: GridProps) => {
   return (
     <Container
       columns={columns}
